b822b801da3f4133464b1ee1d4e69294093ec78b
Vincent Dellong <vince@macbook-pro-de-vincent-2.home>
2018-06-14 11:14:55 +0200

    Add carousel

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+//Carousel parameters
+$(document).ready(function() {
+  $('.carousel').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHove: false
+  });
+});
